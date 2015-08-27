410c8b9775d94078bc3d0335016599acddaab7e6
Alexander Kammerer <alexander.kammerer@online.de>
2015-08-28 00:28:31 +0200

    Updated copyright notice.

--- src/main/java/org/openhab/data/ListElement.java
+++ src/main/java/org/openhab/data/ListElement.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Alexander Kammerer 2015.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution.
+ */
+
 package org.openhab.data;
 
 import org.openhab.models.Model;
